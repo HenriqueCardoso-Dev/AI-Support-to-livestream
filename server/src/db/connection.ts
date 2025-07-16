1680fd7aa833d83872e6891a71a2ef250c228a94
Carlos Henrique Ramos Cardoso <carloshenriqueramoscardoso@gmail.com>
2025-07-15 23:10:01 -0300

    fix: rename file name

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,7 +1,7 @@
 import postgres from 'postgres';
 import { env } from '../env.ts';
 import { drizzle } from 'drizzle-orm/postgres-js'
-import { schema } from './schema/ndex.ts';
+import { schema } from './schema/index.ts';
 
 export const sqlClient = postgres(env.DATABASE_URL) 
 export const db = drizzle(sqlClient, {
