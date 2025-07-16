6d6c2265c8165cd5b10b7703e26f3f6b19975848
Carlos Henrique Ramos Cardoso <carloshenriqueramoscardoso@gmail.com>
2025-07-15 23:01:40 -0300

    feat: add drizzle config and create first migration

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,4 +1,10 @@
 import postgres from 'postgres';
 import { env } from '../env.ts';
+import { drizzle } from 'drizzle-orm/postgres-js'
+import { schema } from './schema/ndex.ts';
 
 export const sqlClient = postgres(env.DATABASE_URL) 
+export const db = drizzle(sqlClient, {
+  schema,
+  casing: 'snake_case'
+})
